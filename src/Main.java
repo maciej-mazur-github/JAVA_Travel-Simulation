a447101a04663aeebbafca6f7e2a921857605f50
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-04-13 18:49:42 +0200

    Add removeCity(String), removeCity(), printActions(), start implementing startTraveling()

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import com.linkedLists.travel.Travel;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/linkedLists/travel/Travel.java
@@ -0,0 +1,97 @@
+package com.linkedLists.travel;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Travel {
+    private LinkedList<String> places = new LinkedList<>();
+    private Scanner scanner = new Scanner(System.in);
+    private ListIterator<String> travelIterator = places.listIterator();
+    private boolean goingForward = true;
+
+    public void startTraveling() {
+        System.out.println("Welcome to our traveling agency. Choose one of below actions:\n");
+        printActions();
+
+        boolean quit = false;
+        int choice;
+
+        while (!quit) {
+            System.out.print("\nWhat is your choice? (5 to print the available options) ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if(choice < 0 || choice > 5) {
+                System.out.println("Invalid choice number. Try again.");
+                continue;
+            }
+
+            switch (choice) {
+                case 0:
+                    System.out.println("You have decided to finish your travel");
+                    quit = true;
+                    break;
+                case 1:
+
+            }
+        }
+    }
+
+    private void removeCity(String cityName) {
+        String nextCityName = cityName;
+
+        if(goingForward) {
+            if(travelIterator.hasNext()) {
+                String currentCityName = travelIterator.next();
+
+                if(currentCityName.equals(cityName)) {
+                    nextCityName = travelIterator.next();
+                } else {
+                    travelIterator.previous();
+                }
+            }
+
+        } else {
+            if(travelIterator.hasPrevious()) {
+                String currentCityName = travelIterator.previous();
+
+                if(currentCityName.equals(cityName)) {
+                    nextCityName = travelIterator.previous();
+                } else {
+                    travelIterator.previous();
+                }
+            }
+        }
+
+        places.remove(cityName);
+        System.out.println(cityName + " successfully removed. You are now in the next city which is " + nextCityName);
+    }
+
+    private boolean removeCity() {
+        System.out.print("Which city would like to remove? ");
+        String cityToRemove = scanner.nextLine();
+
+        if(places.contains(cityToRemove)) {
+            removeCity(cityToRemove);
+            return true;
+        } else {
+            System.out.println("There is no such city in the list like " + cityToRemove);
+            return false;
+        }
+    }
+
+
+
+    private void printActions() {
+        System.out.println("Available options. Press: ");
+        System.out.println("\t0 - to quit the program");
+        System.out.println("\t1 - to go to the next city");
+        System.out.println("\t2 - to go to the previous city");
+        System.out.println("\t3 - to add a new city in ascending order");
+        System.out.println("\t4 - to remove the city");
+        System.out.println("\t5 - to print the available options");
+    }
+
+}
